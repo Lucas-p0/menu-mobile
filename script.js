2536101d70d3e8df13cf5a43b82a4a907d1634fd
Lucas pinheiro <lucaslogos@gmail.com>
2022-11-25 11:00:53 -0300

    UpDate

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 const btnMobile = document.querySelector(".btn-mobile");
 
-function toggleMenu() {
-<<<<<<< HEAD
-    const btnMobile = document.querySelector(".nav");
-
-}
-
-
-
-btnMobile.addEventListener('click', toggleMenu)
-=======
-    const nav = document.querySelector(".nav");
-    nav.classList.toggle('active');
-}
-btnMobile.addEventListener('click', toggleMenu);
->>>>>>> 0b5d03fe3af2e38d37f26be386b2afc5e52f6dac
+function toggleMenu() { }
